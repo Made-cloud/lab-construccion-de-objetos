ca1c59e4bb29465f9ff46fcf8513068b49bed418
Madelaine Reyes <made01271998@gmail.com>
2025-11-06 15:22:22 -0300

    actualizacion de lista de comra

--- task6.js
+++ task6.js
@@ -1,5 +1,9 @@
 
 // Type your code below this line!
+
+//- ¿Cómo recopilarás la cantidad y el nombre de cada producto?
+//recopilo informacion creando items de productos para poder generar una funcion que muestre la lista de compra 
+
  
 // 1. Crear un item
 const item1 = {
@@ -12,10 +16,19 @@ const listacompra = [];
 
 // 3. Función para agregar nuevos productos
 function itemnew(name, quantity) {
+    //- ¿Cómo manejarás artículos repetidos?
+    //  lo hago con una condición de if y else para que sume los valores de los items de productos repetidos 
+    const existente = listacompra.find(item => item.name == name) 
     const item2 = {
         name: name,
         quantity: quantity
     };
+    if(existente){
+        existente.quantity+=quantity;
+    } else {
+        const item2 ={name, quantity};
+    }
+
     listacompra.push(item2); // agregamos el nuevo producto
 }
 
@@ -23,9 +36,13 @@ function itemnew(name, quantity) {
 listacompra.push(item1);
 itemnew("apple", 9);
 itemnew("cookies", 45);
-itemnew("rice", 5);
+itemnew("rice", 5)
+itemnew("butter", 1)
 
-// 5. Mostrar la lista con formato bonito
+
+
+ //- ¿Cómo imprimirás el resumen final de la lista?
+ 
 function mostrarLista() {
     console.log("🛒 Lista de compras:");
     for (let i = 0; i < listacompra.length; i++) {
@@ -34,5 +51,5 @@ function mostrarLista() {
     }
 }
 
-//
+
 mostrarLista();
